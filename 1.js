6bf00e243bec016a3c86976bf2e0c70da7b29f73
Promickey <thinhleduc1692@gmail.com>
2018-11-07 10:29:37 +0700

    khoi san pham

--- 1.js
+++ 1.js
@@ -1,4 +1,17 @@
 ﻿ $(function(){
+ 	vitriedu = $('#khoiedu').offset().top;
+ 	console.log(vitriedu); 
+ 	$(".xuong, .kn").click(function(){
+ 		$('body,html').animate({scrollTop:vitriedu});
+ 		return false;
+ 	})
+
+ 	vitrisanpham = $('#khoisanpham').offset().top;
+ 	console.log(vitriedu); 
+ 	$(".sp").click(function(){
+ 		$('body,html').animate({scrollTop:vitrisanpham});
+ 		return false;
+ 	})
  
 })  
  
