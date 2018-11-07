0e17a07b9ee37d6a7280a11c9471bfd4d304b675
Promickey <thinhleduc1692@gmail.com>
2018-11-07 11:07:36 +0700

    sua comment facebook

--- 1.js
+++ 1.js
@@ -1,15 +1,20 @@
 ﻿ $(function(){
  	vitriedu = $('#khoiedu').offset().top;
- 	console.log(vitriedu); 
  	$(".xuong, .kn").click(function(){
  		$('body,html').animate({scrollTop:vitriedu});
  		return false;
  	})
 
  	vitrisanpham = $('#khoisanpham').offset().top;
- 	console.log(vitriedu); 
  	$(".sp").click(function(){
  		$('body,html').animate({scrollTop:vitrisanpham});
+ 		return false;
+ 	})
+
+ 	vitriphanhoi = $('#phanhoi').offset().top;
+ 	console.log(vitriedu); 
+ 	$(".cm").click(function(){
+ 		$('body,html').animate({scrollTop:vitriphanhoi});
  		return false;
  	})
  
